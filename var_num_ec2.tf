b0e777fe735deed403c866b677538a2d5b311774
Krishna227227 <150412115+Krishna227227@users.noreply.github.com>
2024-07-21 08:03:46 +0530

    Create var_num_ec2.tf

---
+++ var_num_ec2.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  "t2.micro"
+   count = var.instance_count
+
+   tags = {
+           Name = "Terraform EC2"
+   }
+}
+
+variable "instance_count" {
+  description = "EC2 instance count"
+  type        = number
+  default     = 2
+}
