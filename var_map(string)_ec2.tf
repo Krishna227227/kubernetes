ae41f06457c135456668abb903d13d5ded4c6663
Krishna227227 <150412115+Krishna227227@users.noreply.github.com>
2024-07-21 08:26:59 +0530

    Create var_map(string)_ec2.tf

---
+++ var_map(string)_ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  "t2.micro"
+
+   tags = var.project_environment
+
+}
+
+variable "project_environment" {
+  description = "project name and environment"
+  type        = map(string)
+  default     = {
+    project     = "project-alpha",
+    environment = "dev"
+  }
+}
