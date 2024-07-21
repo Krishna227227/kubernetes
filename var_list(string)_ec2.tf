6e3a1a1db35b6d564d2f78c515b99ff88846fd72
Krishna227227 <150412115+Krishna227227@users.noreply.github.com>
2024-07-21 08:21:35 +0530

    Create var_list(string)_ec2.tf

---
+++ var_list(string)_ec2.tf
@@ -0,0 +1,12 @@
+# creating 3 IAM users
+
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
+
+variable "user_names" {
+  description = "IAM usernames"
+  type        = list(string)
+  default     = ["user1", "user2", "user3"]
+}
